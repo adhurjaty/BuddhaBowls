19dac9ef834f887e062757839019dcf8cf6db82d
adhurjaty <adhurjaty@novacoast.com>
2017-03-16 20:23:14 -0700

    added data files - not sure why

---
+++ BuddhaBowls/UserControls/MultiSelectDataGrid.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace BuddhaBowls.UserControls
+{
+    public class MultiSelectDataGrid : DataGrid
+    {
+        public IList SelectedItemsList
+        {
+            get
+            {
+                return (IList)GetValue(SelectedItemsListProperty);
+            }
+            set
+            {
+                SetValue(SelectedItemsListProperty, value);
+            }
+        }
+
+        public static readonly DependencyProperty SelectedItemsListProperty =
+            DependencyProperty.Register("SelectedItemsList", typeof(IList), typeof(MultiSelectDataGrid), new PropertyMetadata(null));
+
+        public MultiSelectDataGrid()
+        {
+            SelectionChanged += MultiSelectDataGrid_SelectionChanged;
+        }
+
+        private void MultiSelectDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            SelectedItemsList = SelectedItems;
+        }
+    }
+}
