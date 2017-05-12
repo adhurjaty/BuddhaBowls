91c28bbcdeac27b0dbb9a5eccd7433f3a8966bd7
adhurjaty <adhurjaty@novacoast.com>
2017-05-12 16:12:40 -0700

    Adds helper and model container tests

---
+++ BuddhaBowls.Test/HelperTests.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BuddhaBowls.Helpers;
+using System.Linq;
+using BuddhaBowls.Services;
+using BuddhaBowls.Models;
+
+namespace BuddhaBowls.Test
+{
+    /// <summary>
+    /// Summary description for HelperTests
+    /// </summary>
+    [TestClass]
+    public class HelperTests
+    {
+        public HelperTests()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        
+        
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            BuddhaBowls.Properties.Settings.Default.DBLocation = Properties.Settings.Default.DBLocation;
+        }
+
+        [TestCleanup()]
+        public void MyTestCleanup()
+        {
+            BuddhaBowls.Properties.Settings.Default.InventoryOrder = null;
+        }
+
+        #endregion
+
+        [TestMethod]
+        public void ListToCsvTest()
+        {
+            List<string[]> contents = new List<string[]>()
+            {
+                new string[] { "Hello", "World", "What!", "are..." },
+                new string[] { "lkasdf", "Worllk;mLKMVd", "kasmdfsfafeiso!", "are..." },
+                new string[] { "asdfn", "help", "What!", "are..." },
+                new string[] { "Hello", "", "What!", "are..." },
+                new string[] { "Hello", "World", "|||", "are..." },
+                new string[] { "Hello", "", "What!", "are..." }
+            };
+
+            string output = MainHelper.ListToCsv(contents);
+
+            string desired = "Hello,World,What!,are...\n" +
+                            "lkasdf,Worllk;mLKMVd,kasmdfsfafeiso!,are...\n" +
+                            "asdfn,help,What!,are...\n" +
+                            "Hello,,What!,are...\n" +
+                            "Hello,World,|||,are...\n" +
+                            "Hello,,What!,are...";
+
+            Assert.AreEqual(desired, output);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Contents cannot have a , in them")]
+        public void ListToCsvErrorTest()
+        {
+            List<string[]> contents = new List<string[]>()
+            {
+                new string[] { "Hello", "World", "What!", "are..." },
+                new string[] { "lkasdf", "Worllk;mLKMVd", "kasm,dfsfafeiso!", "are..." },
+                new string[] { "asdfn", "help", "What!", "are..." },
+                new string[] { "Hello", "", "What!", "are..." },
+                new string[] { "Hello", "World", ",,,", "are..." },
+                new string[] { "Hello", "", "What!", "are..." }
+            };
+
+            MainHelper.ListToCsv(contents);
+        }
+
+        [TestMethod]
+        public void SortItemsNoOrderTest()
+        {
+            List<InventoryItem> unordered = new List<InventoryItem>()
+            {
+                MockObjects.GetInventoryItem("Avocado"),
+                MockObjects.GetInventoryItem("Cinnamon"),
+                MockObjects.GetInventoryItem("Bacon Bits"),
+                MockObjects.GetInventoryItem("Cucumbers"),
+                MockObjects.GetInventoryItem("Celery"),
+                MockObjects.GetInventoryItem("Black Beans"),
+                MockObjects.GetInventoryItem("Dried Basil")
+            };
+
+            BuddhaBowls.Properties.Settings.Default.InventoryOrder = null;
+            List<InventoryItem> ordered = MainHelper.SortItems(unordered).ToList();
+
+            List<InventoryItem> refOrdered = new List<InventoryItem>()
+            {
+                MockObjects.GetInventoryItem("Avocado"),
+                MockObjects.GetInventoryItem("Bacon Bits"),
+                MockObjects.GetInventoryItem("Black Beans"),
+                MockObjects.GetInventoryItem("Celery"),
+                MockObjects.GetInventoryItem("Cinnamon"),
+                MockObjects.GetInventoryItem("Cucumbers"),
+                MockObjects.GetInventoryItem("Dried Basil")
+            };
+
+            CollectionAssert.AreEqual(refOrdered.Select(x => x.Name).ToList(), ordered.Select(x => x.Name).ToList());
+        }
+
+        [TestMethod]
+        public void SortItemsFullOrderTest()
+        {
+            BuddhaBowls.Properties.Settings.Default.InventoryOrder = new List<string>()
+            {
+                "Cinnamon",
+                "Black Beans",
+                "Cucumbers",
+                "Avocado",
+                "Dried Basil",
+                "Bacon Bits",
+                "Celery"
+            };
+
+            List<InventoryItem> unordered = new List<InventoryItem>()
+            {
+                MockObjects.GetInventoryItem("Avocado"),
+                MockObjects.GetInventoryItem("Cinnamon"),
+                MockObjects.GetInventoryItem("Bacon Bits"),
+                MockObjects.GetInventoryItem("Cucumbers"),
+                MockObjects.GetInventoryItem("Celery"),
+                MockObjects.GetInventoryItem("Black Beans"),
+                MockObjects.GetInventoryItem("Dried Basil")
+            };
+
+            List<InventoryItem> ordered = MainHelper.SortItems(unordered).ToList();
+
+            List<InventoryItem> refOrdered = new List<InventoryItem>()
+            {
+                MockObjects.GetInventoryItem("Cinnamon"),
+                MockObjects.GetInventoryItem("Black Beans"),
+                MockObjects.GetInventoryItem("Cucumbers"),
+                MockObjects.GetInventoryItem("Avocado"),
+                MockObjects.GetInventoryItem("Dried Basil"),
+                MockObjects.GetInventoryItem("Bacon Bits"),
+                MockObjects.GetInventoryItem("Celery")
+            };
+
+            CollectionAssert.AreEqual(refOrdered.Select(x => x.Name).ToList(), ordered.Select(x => x.Name).ToList());
+        }
+
+        [TestMethod]
+        public void SortItemsPartialOrderTest()
+        {
+            BuddhaBowls.Properties.Settings.Default.InventoryOrder = new List<string>()
+            {
+                "Cinnamon",
+                "Black Beans",
+                "Cucumbers"
+            };
+
+            List<InventoryItem> unordered = new List<InventoryItem>()
+            {
+                MockObjects.GetInventoryItem("Avocado"),
+                MockObjects.GetInventoryItem("Cinnamon"),
+                MockObjects.GetInventoryItem("Bacon Bits"),
+                MockObjects.GetInventoryItem("Cucumbers"),
+                MockObjects.GetInventoryItem("Celery"),
+                MockObjects.GetInventoryItem("Black Beans"),
+                MockObjects.GetInventoryItem("Dried Basil")
+            };
+
+            List<InventoryItem> ordered = MainHelper.SortItems(unordered).ToList();
+
+            List<InventoryItem> refOrdered = new List<InventoryItem>()
+            {
+                MockObjects.GetInventoryItem("Cinnamon"),
+                MockObjects.GetInventoryItem("Black Beans"),
+                MockObjects.GetInventoryItem("Cucumbers"),
+                MockObjects.GetInventoryItem("Avocado"),
+                MockObjects.GetInventoryItem("Bacon Bits"),
+                MockObjects.GetInventoryItem("Celery"),
+                MockObjects.GetInventoryItem("Dried Basil")
+            };
+
+            CollectionAssert.AreEqual(refOrdered.Select(x => x.Name).ToList(), ordered.Select(x => x.Name).ToList());
+        }
+
+        [TestMethod]
+        public void CategoryGroupingTest()
+        {
+            BuddhaBowls.Properties.Settings.Default.InventoryOrder = new List<string>()
+            {
+                "Cinnamon",
+                "Black Beans",
+                "Cucumbers",
+                "Avocado",
+                "Dried Basil",
+                "Bacon Bits",
+                "Celery"
+            };
+
+            List<InventoryItem> unordered = new List<InventoryItem>()
+            {
+                MockObjects.GetInventoryItem("Avocado"),
+                MockObjects.GetInventoryItem("Cinnamon"),
+                MockObjects.GetInventoryItem("Bacon Bits"),
+                MockObjects.GetInventoryItem("Cucumbers"),
+                MockObjects.GetInventoryItem("Celery"),
+                MockObjects.GetInventoryItem("Black Beans"),
+                MockObjects.GetInventoryItem("Dried Basil")
+            };
+
+            List<IGrouping<string, InventoryItem>> group = MainHelper.CategoryGrouping(unordered).ToList();
+
+            List<string> refCategoryOrder = new List<string>()
+            {
+                "Herbs",
+                "Dry Goods",
+                "Produce",
+                "Meats"
+            };
+
+            List<IGrouping<string, InventoryItem>> refGroup = new List<InventoryItem>()
+            {
+                // Herbs
+                MockObjects.GetInventoryItem("Cinnamon"),
+                MockObjects.GetInventoryItem("Dried Basil"),
+                // Dry Goods
+                MockObjects.GetInventoryItem("Black Beans"),
+                // Produce
+                MockObjects.GetInventoryItem("Cucumbers"),
+                MockObjects.GetInventoryItem("Avocado"),
+                MockObjects.GetInventoryItem("Celery"),
+                // Meats
+                MockObjects.GetInventoryItem("Bacon Bits")
+            }.GroupBy(x => x.Category).ToList();
+
+            CollectionAssert.AreEqual(refCategoryOrder, group.Select(x => x.Key).ToList());
+
+            
+            foreach (IGrouping<string, InventoryItem> refItem in refGroup)
+            {
+                foreach(IGrouping<string, InventoryItem> compItem in group)
+                {
+                    if (refItem.Key == compItem.Key)
+                    {
+                        CollectionAssert.AreEqual(refItem.Select(x => x.Name).ToList(), compItem.Select(x => x.Name).ToList());
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ BuddhaBowls.Test/ModelContainerTests.cs
@@ -0,0 +1,327 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BuddhaBowls.Models;
+using BuddhaBowls.Helpers;
+using BuddhaBowls.Services;
+using System.Linq;
+using System.IO;
+
+namespace BuddhaBowls.Test
+{
+    /// <summary>
+    /// Summary description for ModelContainerTests
+    /// </summary>
+    [TestClass]
+    public class ModelContainerTests
+    {
+        private ModelContainer _models;
+
+        public ModelContainerTests()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        [TestInitialize()]
+        public void MyTestInitialize()
+        {
+            BuddhaBowls.Properties.Settings.Default.DBLocation = Properties.Settings.Default.DBLocation;
+            _models = new ModelContainer();
+        }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void GetIngredientsTest()
+        {
+            // commenting this out probably destroys the test, but I don't want outside classes setting InventoryItems
+            _models.InventoryItems = _models.InventoryItems.Take(5).ToList();
+            _models.Recipes = _models.Recipes.Take(4).ToList();
+
+            List<IItem> allItems = _models.GetAllIItems();
+
+            List<IItem> refList = new List<IItem>()
+            {
+                new InventoryItem(new Dictionary<string, string>() { { "Name", "Mozzarella" } }),
+                new InventoryItem(new Dictionary<string, string>() { { "Name", "Cheddar" } }),
+                new InventoryItem(new Dictionary<string, string>() { { "Name", "Feta" } }),
+                new InventoryItem(new Dictionary<string, string>() { { "Name", "Butter" } }),
+                new InventoryItem(new Dictionary<string, string>() { { "Name", "Sour Cream" } }),
+                new Recipe(new Dictionary<string, string>() { { "Name", "Mac & Cheese" } }),
+                new Recipe(new Dictionary<string, string>() { { "Name", "Balsamic Vinegar" } }),
+                new Recipe(new Dictionary<string, string>() { { "Name", "Chicken Marinade" } }),
+                new Recipe(new Dictionary<string, string>() { { "Name", "Chili" } })
+            };
+
+            CollectionAssert.AreEqual(refList.Select(x => x.Name).ToList(), allItems.Select(x => x.Name).ToList());
+        }
+
+        [TestMethod]
+        public void GetCountUnitsTest()
+        {
+            List<string> countUnits = _models.GetCountUnits();
+
+            List<string> refCountUnits = new List<string>()
+            {
+                "5lb BAG",
+                "1lb",
+                "5lb BUCKET",
+                "1/2 GALLON",
+                "TRAY",
+                "GALLON",
+                "QUART",
+                "BUCKET",
+                "CAN",
+                "JUG",
+                "BAG",
+                "6\" PAN",
+                "JAR",
+                "BOTTLE",
+                "EACH",
+                "LB",
+                "GAL",
+                "OZ-wt",
+                "Container",
+                "1/2 CASE",
+                "FLAT",
+                "CASE",
+                "BASKET",
+                "LOG"
+            };
+
+            CollectionAssert.AreEquivalent(refCountUnits, countUnits);
+        }
+
+        [TestMethod]
+        public void GetRecipeUnitsTest()
+        {
+            List<string> recUnits = _models.GetRecipeUnits();
+
+            List<string> refrecUnits = new List<string>()
+            {
+                "OZ-wt",
+                "CUP",
+                "EA",
+                "OZ-fl",
+                "TBL",
+                "BUNCH"
+            };
+
+            CollectionAssert.AreEquivalent(refrecUnits, recUnits);
+        }
+
+        [TestMethod]
+        public void AddInventoryItemTest()
+        {
+            string name = "New Test Item";
+            InventoryItem testItem = new InventoryItem() { Name = name };
+            int origSize = _models.InventoryItems.Count;
+
+            InventoryItem dbItem;
+            try
+            {
+                _models.AddUpdateInventoryItem(ref testItem);
+
+                CollectionAssert.Contains(_models.InventoryItems, testItem);
+                Assert.AreEqual(origSize, testItem.Id);
+                dbItem = ModelHelper.InstantiateList<InventoryItem>("InventoryItem").First(x => x.Name == name);
+                Assert.AreEqual(origSize, dbItem.Id);
+            }
+            finally
+            {
+                _models.DeleteInventoryItem(testItem);
+            }
+
+            CollectionAssert.DoesNotContain(_models.InventoryItems, testItem);
+            dbItem = new InventoryItem(new Dictionary<string, string>() { { "Name", name } });
+            Assert.IsNull(dbItem.Name);
+            CollectionAssert.DoesNotContain(BuddhaBowls.Properties.Settings.Default.InventoryOrder, name);
+
+        }
+
+        [TestMethod]
+        public void UpdateInventoryItemTest()
+        {
+            InventoryItem testItem = _models.InventoryItems.First();
+            float tempOrderAmt = testItem.LastOrderAmount;
+            int refId = testItem.Id;
+            int refCount = _models.InventoryItems.Count;
+            testItem.LastOrderAmount = 77;
+
+            try
+            {
+                _models.AddUpdateInventoryItem(ref testItem);
+
+                Assert.AreSame(_models.InventoryItems.First(), testItem);
+                Assert.AreEqual(77, testItem.LastOrderAmount);
+                Assert.AreEqual(refId, testItem.Id);
+                Assert.AreEqual(refCount, _models.InventoryItems.Count);
+                InventoryItem dbItem = ModelHelper.InstantiateList<InventoryItem>("InventoryItem").FirstOrDefault(x => x.Name == testItem.Name);
+                Assert.AreEqual(77, dbItem.LastOrderAmount);
+            }
+            finally
+            {
+                testItem.LastOrderAmount = tempOrderAmt;
+                _models.AddUpdateInventoryItem(ref testItem);
+            }
+        }
+
+        [TestMethod]
+        public void AddVendorTest()
+        {
+            string name = "New Test Vendor";
+            Vendor testVendor = new Vendor() { Name = name };
+
+            try
+            {
+                _models.AddUpdateVendor(ref testVendor);
+
+                CollectionAssert.Contains(_models.Vendors, testVendor);
+                Assert.AreEqual(5, testVendor.Id);
+                Vendor dbItem = ModelHelper.InstantiateList<Vendor>("Vendor").First(x => x.Name == name);
+                Assert.AreEqual(5, dbItem.Id);
+            }
+            finally
+            {
+                _models.DeleteVendor(testVendor);
+            }
+            CollectionAssert.DoesNotContain(_models.InventoryItems, testVendor);
+            Vendor newDbItem = ModelHelper.InstantiateList<Vendor>("InventoryItem").FirstOrDefault(x => x.Name == name);
+            Assert.IsNull(newDbItem);
+        }
+
+        [TestMethod]
+        public void UpdateVendorTest()
+        {
+            Vendor testVendor = _models.Vendors.First();
+            string tempPhone = testVendor.PhoneNumber;
+            int refId = testVendor.Id;
+            int refCount = _models.Vendors.Count;
+            string phone = "8887777";
+            testVendor.PhoneNumber = phone;
+
+            _models.AddUpdateVendor(ref testVendor);
+
+            Assert.AreSame(_models.Vendors.First(), testVendor);
+            Assert.AreEqual(phone, testVendor.PhoneNumber);
+            Assert.AreEqual(refId, testVendor.Id);
+            Assert.AreEqual(refCount, _models.Vendors.Count);
+            Vendor dbItem = ModelHelper.InstantiateList<Vendor>("Vendor").FirstOrDefault(x => x.Name == testVendor.Name);
+            Assert.AreEqual(phone, dbItem.PhoneNumber);
+
+            testVendor.PhoneNumber = tempPhone;
+            _models.AddUpdateVendor(ref testVendor);
+        }
+
+        [TestMethod]
+        public void GetVendorsFromItemTest()
+        {
+            InventoryItem testItem = _models.InventoryItems.First(x => x.Name == "Cheddar");
+
+            Dictionary<Vendor, InventoryItem> testDict = _models.GetVendorsFromItem(testItem);
+            List<Vendor> keyList = new List<Vendor>(testDict.Keys);
+
+            Assert.AreEqual(2, keyList.Count);
+            Assert.AreEqual(7, testDict[keyList.First(x => x.Name == "Another guy")].LastOrderAmount);
+            Assert.AreEqual(3, testDict[keyList.First(x => x.Name == "Sysco")].LastOrderAmount);
+        }
+
+        [TestMethod]
+        public void GetEmptyVendorsFromItemTest()
+        {
+            InventoryItem testItem = _models.InventoryItems.First(x => x.Name == "Cookie Dough");
+
+            Dictionary<Vendor, InventoryItem> testDict = _models.GetVendorsFromItem(testItem);
+            List<Vendor> keyList = new List<Vendor>(testDict.Keys);
+
+            Assert.IsNull(keyList[0].Name);
+            //Assert.AreEqual(0, keyList.Count);
+        }
+
+        [TestMethod]
+        public void GetCategoryValuesTest()
+        {
+            _models.InventoryItems = new List<InventoryItem>()
+            {
+                new InventoryItem() { Name = "Sourdough", Category = "Bread", LastPurchasedPrice = 5f, Conversion = 1, Count = 10, Yield = 1 },
+                new InventoryItem() { Name = "Wheat", Category = "Bread", LastPurchasedPrice = 6f, Conversion = 1, Count = 10, Yield = 1 },
+                new InventoryItem() { Name = "Milk", Category = "Dairy", LastPurchasedPrice = 7f, Conversion = 1, Count = 10, Yield = 1 },
+                new InventoryItem() { Name = "Cheese", Category = "Dairy", LastPurchasedPrice = 8f, Conversion = 1, Count = 10, Yield = 1 },
+                new InventoryItem() { Name = "Celery", Category = "Produce", LastPurchasedPrice = 9f, Conversion = 1, Count = 10, Yield = 1 }
+            };
+
+            _models.PrepItems = new List<PrepItem>()
+            {
+                new PrepItem() { Name = "Sourdough",Cost = 5f, LineCount = 1 },
+                new PrepItem() { Name = "Wheat", Cost = 6f, WalkInCount = 1 },
+                new PrepItem() { Name = "Milk", Cost = 1f, WalkInCount = 5, LineCount = 10 },
+                new PrepItem() { Name = "Cheese", Cost = 1f, LineCount = 10, WalkInCount = 10 },
+            };
+
+            Dictionary<string, float> refCostDict = new Dictionary<string, float>()
+            {
+                { "Bread", 55f + 66f  },
+                { "Dairy", 185f  },
+                { "Produce", 90f  },
+            };
+
+            Dictionary<string, float> costDict = _models.GetCategoryValues();
+
+            CollectionAssert.AreEquivalent(refCostDict, costDict);
+        }
+
+        [TestMethod]
+        public void CreateTestObjects()
+        {
+            //List<InventoryItem> items = ModelHelper.InstantiateList<InventoryItem>("InventoryItem");
+            //foreach (InventoryItem item in items)
+            //{
+            //    MockObjects.SaveInventoryItem(item);
+            //}
+
+            //List<Recipe> recipes = ModelHelper.InstantiateList<Recipe>("Recipe");
+            //foreach (Recipe rec in recipes)
+            //{
+            //    MockObjects.SaveRecipe(rec);
+            //}
+        }
+    }
+}
